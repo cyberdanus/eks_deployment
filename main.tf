67b734990ac4e93465075321aed205a35b411806
Dany <forextd@gmail.com>
2023-01-26 17:33:29 +0500

    some_fix

--- main.tf
+++ main.tf
@@ -50,24 +50,24 @@ terraform {
 }
 
 
-resource "aws_s3_bucket" "my-tform-state" {
-  bucket = "my-tform-state"
-}
+# resource "aws_s3_bucket" "my-tform-state" {
+#   bucket = "my-tform-state"
+# }
 
-resource "aws_s3_bucket_acl" "my-tform-state" {
-  bucket = aws_s3_bucket.my-tform-state.id
-  acl    = "private"
-}
+# resource "aws_s3_bucket_acl" "my-tform-state" {
+#   bucket = aws_s3_bucket.my-tform-state.id
+#   acl    = "private"
+# }
 
-resource "aws_dynamodb_table" "my-tform-lock" {
-  name           = "my-tform-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+# resource "aws_dynamodb_table" "my-tform-lock" {
+#   name           = "my-tform-lock"
+#   billing_mode   = "PAY_PER_REQUEST"
+#   hash_key       = "LockID"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
 ##4bucket
 
 data "aws_availability_zones" "available" {}
