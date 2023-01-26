5a3b80f1e329cb75f7540025ae28292dd57dcd49
Dany <forextd@gmail.com>
2023-01-26 14:14:30 +0500

    add_vars

--- variables.tf
+++ variables.tf
@@ -3,3 +3,23 @@ variable "region" {
   type        = string
   default     = "eu-central-1"
 }
+variable "private_subnet_cidrs" {
+  description = "VPC private subnets"
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24"
+  ]
+}
+variable "public_subnet_cidrs" {
+  description = "VPC public subnets"
+  default = [
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/24"
+  ]
+}
+variable "cidrs" {
+  description = "VPC network"
+  default = "10.0.0.0/16"
+}
